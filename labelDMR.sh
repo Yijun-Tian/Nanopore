7168def4cd48f75317530e4429cf0ccbab5e8a3e
Yijun-Tian <56854730+Yijun-Tian@users.noreply.github.com>
2024-10-08 12:09:29 -0400

    Add files via upload
    find DMR from labeled BAM alignments (output from label.py)

---
+++ labelDMR.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# before run
+# ml load BEDTools/2.30.0-GCC-11.2.0
+# ml load Python/3.8.6-GCCcore-10.2.0
+# ml load SAMtools/1.17-GCC-12.2.0
+# Check for the correct number of arguments
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <input.vcf> <input.bam> <output.dmr.txt>"
+    exit 1
+fi
+
+# Assign arguments to variables
+input_vcf="$1"
+input_bam="$2"
+output="$3"
+
+# Temporary file to store processed variants
+temp_variants_file=$(mktemp)
+touch $output
+# Read the VCF line by line
+while IFS= read -r line; do
+    # Skip comment lines
+    if [[ $line =~ ^# ]]; then
+        continue
+    fi
+
+    # Extract fields from the VCF line
+    IFS=$'\t' read -r chr pos id ref alt <<< "$line"
+
+    # Adjust position by subtracting 1
+    adjusted_pos=$((pos - 1))
+
+    # Get the alternate alleles into an array
+    IFS=',' read -r -a alt_array <<< "$alt"
+
+    # Create a variant string in the required format
+    variant_string="$id:$ref:${alt_array[*]}:$chr:$adjusted_pos"
+
+    # Run the Python script for the current variant
+    python label.py -i "$input_bam" -o $id.bam -v "$variant_string"
+    samtools index $id.bam
+
+    # modkit call methylation for the labeled bam ($id.bam)
+    ~/dist/modkit pileup -t 2 --partition-tag VA --partition-tag RG --ref ../hg38.alignmt.fa --only-tabs --cpg $id.bam --combine-strands $id.bg/
+
+
+    cd $id.bg/
+    for j in $(ls *.sorted.bed | rev | cut -c 12- | rev | sort | uniq );
+        do
+	  awk '{FS=OFS="\t"} $4~/m/ && $5>=5 {print $1,$2,$3,$11}' $j.sorted.bed | bedtools sort -i - > $j.bg
+        done
+    	grp1=$(ls ${id}@${ref}_*.bg | paste -sd,)
+    	n1=$(echo $grp1 | sed 's/,/\n/g' | wc -l)
+    	grp2=$(ls ${id}@${alt_array[0]}_*.bg | paste -sd,)
+    	n2=$(echo $grp2 | sed 's/,/\n/g' | wc -l)
+    if [ "${n1}" -ge 3 ] && [ "${n2}" -ge 3 ]; then
+        perl ../../metilene_v0.2-8/metilene_input.pl --in1 $grp1 --in2 $grp2 --out $id.ref.alt.5mCG.input --h1 ref --h2 alt
+        ../../metilene_v0.2-8/metilene_linux64 -t 8 -a ref -b alt -M 2000 -m 3 $id.ref.alt.5mCG.input > $id.ref.alt.5mCG.output 
+        awk -v va=$variant_string -v N1=$n1 -v N2=$n2 '{FS=OFS="\t"}{if ($7<=0.05 || $8<=0.05) print $0,va,N1,N2}' $id.ref.alt.5mCG.output >> ../$output
+        cd ..
+        mv $id.bg/*.input ./
+        rm -rf $id.bg/
+        rm $id.bam
+        rm $id.bam.bai
+     else
+        rm -rf $id.bg/
+        rm $id.bam
+        rm $id.bam.bai
+        cd ..
+     fi
+ 
+
+done < "$input_vcf"
+
+# Clean up
+rm "$temp_variants_file"
+
+echo "Finished processing VCF"
